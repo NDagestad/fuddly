84ceb1e61caeeed4325bc123a41534fc00992022
Nicolai Dagestad <nicolai@dagestad.fr>
2024-10-20 17:45:41 +0200

    Rework the preferred structure of projects
    The new structure could be something like this:
    <prj name>/
      - __init__.py
      - prj.py (project=Project() instance, logger=Logger() instance, targets=..., other
        calls to project.fct_x() and attributes filling like default_dm --- dm_1, ... dm_n)
      - targets.py (definition of specific Targets)
      - monitoring.py  (definition of probes, specific feedback handlers)
      - scenarios.py
      - scripts/__init__.py
      - samples/{dm_1, ... dm_n}
    
    Wwhat really matters is actually just that the project, targets, and logger variables
    are available from the root of the module, the internal organisation is up to the
    user.

--- src/fuddly/projects/tuto/prj.py
+++ src/fuddly/projects/tuto/prj.py
@@ -21,12 +21,9 @@
 #
 ################################################################################
 
-import socket
 
 from fuddly.framework.comm_backends import Serial_Backend
 from fuddly.framework.plumbing import *
-from fuddly.framework.targets.debug import TestTarget
-from fuddly.framework.targets.network import NetworkTarget
 from fuddly.framework.knowledge.information import *
 from fuddly.framework.knowledge.feedback_handler import TestFbkHandler
 from fuddly.framework.scenario import *
@@ -52,117 +49,6 @@ project.add_knowledge(
 )
 
 project.register_feedback_handler(TestFbkHandler())
-
-### TARGETS DEFINITION ###
-
-class TutoNetTarget(NetworkTarget):
-
-    def _custom_data_handling_before_emission(self, data_list):
-        self.listen_to('localhost', 64001, 'Dynamic server interface')
-        # self.connect_to('localhost', 64002, 'Dynamic client interface')
-        # self._logger.collect_feedback('TEST', status_code=random.randint(-2,2))
-        return data_list
-
-    def _feedback_handling(self, fbk, ref):
-        # self.remove_all_dynamic_interfaces()
-        ok_status = 0
-        return fbk, ok_status
-
-tuto_tg = TutoNetTarget(host='localhost', port=12345, data_semantics='TG1', hold_connection=True)
-tuto_tg.register_new_interface('localhost', 54321, (socket.AF_INET, socket.SOCK_STREAM), 'TG2',
-                               server_mode=True, hold_connection=True)
-tuto_tg.add_additional_feedback_interface('localhost', 7777, (socket.AF_INET, socket.SOCK_STREAM),
-                                          fbk_id='My Feedback Source', server_mode=True)
-tuto_tg.set_timeout(fbk_timeout=5, sending_delay=2)
-
-net_tg = NetworkTarget(host='localhost', port=12345,
-                       socket_type=(socket.AF_INET, socket.SOCK_STREAM),
-                       hold_connection=True, server_mode=False, keep_first_client=False)
-
-udpnet_tg = NetworkTarget(host='localhost', port=12345,
-                          socket_type=(socket.AF_INET, socket.SOCK_DGRAM),
-                          hold_connection=True, server_mode=False)
-
-udpnetsrv_tg = NetworkTarget(host='localhost', port=12345,
-                          socket_type=(socket.AF_INET, socket.SOCK_DGRAM),
-                          hold_connection=True, server_mode=True)
-
-ETH_P_ALL = 3
-rawnetsrv_tg = NetworkTarget(host='eth0', port=ETH_P_ALL,
-                             socket_type=(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(ETH_P_ALL)),
-                             hold_connection=True, server_mode=False)
-rawnetsrv_tg.register_new_interface(host='eth2', port=ETH_P_ALL,
-                                    socket_type=(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(ETH_P_ALL)),
-                                    data_semantics='TG2')
-
-### PROBE DEFINITION ###
-
-@probe(project)
-class P1(Probe):
-
-    def start(self, dm, target, logger):
-        self.cpt = 0
-
-    def main(self, dm, target, logger):
-        self.cpt += 1
-
-        return ProbeStatus(self.cpt, info='This is a Linux OS!')
-
-
-@probe(project)
-class P2(Probe):
-
-    def start(self, dm, target, logger):
-        self.cpt = 10
-
-    def main(self, dm, target, logger):
-        self.cpt -= 1
-        ps = ProbeStatus(self.cpt)
-        ps.set_private_info('always KO!')
-        return ps
-
-
-@blocking_probe(project)
-class health_check(Probe):
-
-    def start(self, dm, target, logger):
-        self.cpt = 0
-
-    def stop(self, dm, target, logger):
-        pass
-
-    def main(self, dm, target, logger):
-        self.cpt += 1
-
-        status = 0
-        if self.cpt > 10:
-            status = -1
-
-        return ProbeStatus(status)
-
-if serial_module:
-    serial_backend = Serial_Backend('/dev/ttyUSB0', username='test', password='test', slowness_factor=8)
-
-    @blocking_probe(project)
-    class probe_pid(ProbePID):
-        backend = serial_backend
-        process_name = 'bash'
-
-    @probe(project)
-    class probe_mem(ProbeMem):
-        backend = serial_backend
-        process_name = 'bash'
-        tolerance = 1
-
-### TARGETS ALLOCATION ###
-
-targets = [(EmptyTarget(), (P1, 2), (P2, 1.4), health_check),
-           tuto_tg, net_tg, udpnet_tg, udpnetsrv_tg, rawnetsrv_tg,
-           TestTarget(fbk_samples=['CRC error', 'OK']),
-           TestTarget()]
-
-if serial_module:
-    targets.append((TestTarget(), probe_pid, (probe_mem, 0.2)))
 
 ### PROJECT SCENARIOS DEFINITION ###
 
