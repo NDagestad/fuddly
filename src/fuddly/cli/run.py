a286d65b7e4019dc3a5da52318bd5c3a2b6ecf92
Nicolai Dagestad <nicolai.github@dagestad.fr>
2024-10-21 18:58:54 +0200

    Implement the run command

--- src/fuddly/cli/run.py
+++ src/fuddly/cli/run.py
@@ -1,26 +1,25 @@
 import fuddly.cli.argparse_wrapper as argparse
 from fuddly.cli.error import CliException
+from importlib.util import find_spec
+from importlib.metadata import entry_points
+import fuddly.framework.global_resources as gr
 import sys
 import os.path
-
-
-
+import os
 
 def get_scripts() -> list():
-    import fuddly.framework.global_resources as gr
-    from importlib.metadata import entry_points
-    import pkgutil
-
     paths = []
+
+    # User scripts
     script_dir = os.path.join(gr.fuddly_data_folder, "projects_scripts")
     if os.path.isdir(script_dir):
         path, _, files = next(os.walk(script_dir))
         for f in files:
-            paths.append("fuddly.project_scripts." + f.removesuffix(".py"))
+            paths.append("fuddly.projects_scripts." + f.removesuffix(".py"))
 
-
+    # Third party/modules
     for ep in entry_points(group=gr.ep_group_names["projects"]):
-        p = pkgutil.get_loader(ep.module).path
+        p = find_spec(ep.module).origin
         if os.path.basename(p) == "__init__.py":
             p=os.path.dirname(p)
         else:
@@ -28,31 +27,60 @@ def get_scripts() -> list():
             continue
         if os.path.isdir(os.path.join(p, "scripts")):
             for f in next(os.walk(os.path.join(p, "scripts")))[2]:
-                if f.endswith(".py")  and f != "__init__.py":
-                    paths.append(ep.module + ".script." + f.removesuffix(".py"))
+                if f.endswith(".py") and f != "__init__.py":
+                    paths.append(ep.module + ".scripts." + f.removesuffix(".py"))
 
     return paths
+
+def script_from_pkg_name(name) -> str:
+    # pkg_name.script.name -> ("pkg_name.script", "name.py")
+    *pkg, file = name.split('.')
+    file+=".py"
+    pkg=".".join(pkg)
+
+    # User scripts
+    if pkg.startswith("fuddly.projects_scripts"):
+        path=os.path.join(gr.fuddly_data_folder, "projects_scripts", file)
+        if os.path.isfile(path):
+            return path
+        else:
+            return None
+
+    # Third party/module scripts
+    try:
+        path=find_spec(name).origin
+    except ModuleNotFoundError:
+        return None
+    return path
+
 
 # TODO why does this not work 😥
 def script_argument_completer(prefix, parsed_args, **kwargs):
     print(parsed_args, file=out)
     return ["HAAAAAA"]
 
+
 def start(args: argparse.Namespace):
-    sys.argv=[]
-    sys.argv.append(args.script)
-    sys.argv.extend(args.args[0]) # Why is this an array, that weird - someting to do with nargs
-
-    script_dir = "/tmp"
-    sys.path.append(script_dir)
-
     if args.list:
         for i in get_scripts():
             print(i)
         return 0
 
     if args.script is None:
-        raise CliException("Missing tool name")
+        raise CliException("Missing script name")
 
-    # TODO execute the script from the script directory
-    raise NotImplementedError("Not implemented yet")
+    script = script_from_pkg_name(args.script)
+    if script == None:
+        print(f"Script {args.script} not foud")
+        sys.exit(1)
+
+    argv = args.args[0] # Why is this an array, that weird - someting to do with nargs
+    argv.insert(0, script)
+
+
+    # TODO Add ipython option
+    executor = "python"
+    argv.insert(0, executor)
+
+    os.execvp(executor, argv)
+
